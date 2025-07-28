16852fa4e5a1814a2e0fc2e003c503904ca52e05
upsauniversidad <marinaalvarez@upsa.edu.bo>
2025-07-28 18:26:48 -0400

    Add files via upload

--- riesgo_financiero.py
+++ riesgo_financiero.py
@@ -2,24 +2,24 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.ensemble impor RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.processing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import classification_report, confusion_matrix
 
-#mostrar datos
-ds = pd.read_csv("dataset_financiero_riesgo.csv")
+# Mostrar o cargar los datos
+#ds = pd.read_csv("dataset_financiero_riesgo.csv")
 
-#Colocar un título principal en la página web
+# Colocar un titulo principal en la página Web
 st.title("Predicción de Riesgo Financiero")
 
-#Cargar los datos en la memoriaCACHE para mejorar la velocidad del acceso 
-#al conjunto de datos
-@st.cache_data
+# Cargar los datos en la memoria CACHE para mejorar la velocidad del acceso al 
+# conjunto de datos
+@st.cache_data 
 
-#Hacemos una función q se llama cargar_datos. Leemos el archivo en una variable
-#y retornamos la variable alq llama a la función. En este caso la variable
-#que retornamos se llama "ds", abreviatura de "dataset"
+# Hacemos una funcion que se llama cargar_datos. Leemos el archivo en una variable
+# y retornamos la variable al que llama a la función. En este caso la variable 
+# que retornamos se llama "ds", abreviatura de "dataset".
 def cargar_datos():
     ds = pd.read_csv("dataset_financiero_riesgo.csv")
     return ds
